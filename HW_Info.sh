e5f20976287d9acb29c1d988a4a3ecdbd048396d
Paul Moody <paul.moody1330@gmail.com>
2020-09-27 17:03:05 -0500

    added file system output
    I also added some comments and spacing.

--- HW_Info.sh
+++ HW_Info.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "Hostname: $(hostname)"
-echo "OS Information: $(cat /etc/issue)"
-lsb_release -a
+#echo "OS Information: $(cat /etc/issue)"
+lsb_release -ircd
+echo $'\n'
+#List media sorted by File System (2 is Type) (3 is Size)
+echo "FS information without snap:"
+df -HlT -x squashfs | tail -n +2 | sort -k 1
+echo $'\n'
 sudo lshw -short
+

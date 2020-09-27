a543cfb9f5a7693f19a463b0af9253eb464a9db1
Paul Moody <paul.moody1330@gmail.com>
2020-09-26 21:03:29 -0500

    Changed to ask for the file name.

--- sshkey.sh
+++ sshkey.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 read -p 'Please enter your password: ' pw
-ssh-keygen -t rsa -b 4096 -C "test@test.com" -P $pw -f "/home/pmoody/.ssh/test" -q
+read -p 'Please enter the file name you want: ' KeyFile
+ssh-keygen -t rsa -b 4096 -C "test@test.com" -P $pw -f "/home/pmoody/.ssh/$KeyFile" -q

467fe202b5fcb66f60a1c5a319e983bd5323fa0c
Paul Moody <paul.moody1330@gmail.com>
2020-11-30 10:39:49 -0600

    Added a check for Red Hat release info.

--- HW_Info.sh
+++ HW_Info.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "Hostname: $(hostname)"
 #echo "OS Information: $(cat /etc/issue)"
+FILE='/etc/redhat-release'
+if [ -d "$FILE" ]; then
+	cat $FILE
+fi	
 lsb_release -ircd
 echo $'\n'
 #List media sorted by File System (2 is Type) (3 is Size)

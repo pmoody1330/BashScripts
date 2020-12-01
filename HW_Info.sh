21b4fbaf4fcf928c9a7d1b786d111b2a070c3fd1
Paul Moody <paul.moody1330@gmail.com>
2020-11-30 19:25:44 -0600

    Added a test for /etc/issue

--- HW_Info.sh
+++ HW_Info.sh
@@ -2,9 +2,14 @@
 echo "Hostname: $(hostname)"
 #echo "OS Information: $(cat /etc/issue)"
 FILE='/etc/redhat-release'
+# Check if file exists
 if [ -d "$FILE" ]; then
 	cat $FILE
 fi	
+# Verify that /etc/issue exists and show the contents
+if [ -f /etc/issue ]; then 
+	cat /etc/issue
+fi
 lsb_release -ircd
 echo $'\n'
 #List media sorted by File System (2 is Type) (3 is Size)

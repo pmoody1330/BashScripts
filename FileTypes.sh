a502431392a914a0d0aa45e6d59abe8f4ffd1106
Paul Moody <paul.moody1330@gmail.com>
2021-01-09 12:31:01 -0600

    updated to not use perl

--- FileTypes.sh
+++ FileTypes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u
+find . -type f -name "*.*" | grep -o -E "\.[^\.]+$" | grep -o -E "[[:alpha:]]{2,16}" | awk '{print tolower($0)}' | sort | uniq --count 

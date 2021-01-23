ac3b1747b06d8b8c60e9bf31dbefe330bb7f6d52
Paul Moody <paul.moody1330@gmail.com>
2021-01-23 13:03:26 -0600

    I added Terraform
    I commented out all of the Tessitura repos.

--- Pull-Repos.sh
+++ Pull-Repos.sh
@@ -10,38 +10,41 @@ reset=`tput sgr0`
 #}
 
 
-#Powershell_Private repo
-cd /home/pmoody/repositories/powershell_private
-p=$(pwd)
-echo "${magenta}$p"
-remote=$(git remote -v)
-echo "${orange}$remote ${reset}"
-git status
-git pull
+# Powershell_Private my private repo
+#cd /home/pmoody/repositories/powershell_private
+#p=$(pwd)
+#echo "${magenta}$p"
+#remote=$(git remote -v)
+#echo "${orange}$remote ${reset}"
+#git status
+#git pull
 
-cd /home/pmoody/repositories/ramp_sql-scripts
-p=$(pwd)
-echo "${magenta}$p"
-remote=$(git remote -v)
-echo "${orange}$remote ${reset}"
-git status
-git pull
+# Legacy Scripts
+#cd /home/pmoody/repositories/ramp_sql-scripts
+#p=$(pwd)
+#echo "${magenta}$p"
+#remote=$(git remote -v)
+#echo "${orange}$remote ${reset}"
+#git status
+#git pull
 
-cd /home/pmoody/repositories/scripts
-p=$(pwd)
-echo "${magenta}$p"
-remote=$(git remote -v)
-echo "${orange}$remote ${reset}"
-git status
-git pull
+# Public Scripts for support (Read Only db access)
+#cd /home/pmoody/repositories/scripts
+#p=$(pwd)
+#echo "${magenta}$p"
+#remote=$(git remote -v)
+#echo "${orange}$remote ${reset}"
+#git status
+#git pull
 
-cd /home/pmoody/repositories/tnhs-gocd-scripts
-p=$(pwd)
-echo "${magenta}$p"
-remote=$(git remote -v)
-echo "${orange}$remote ${reset}"
-git status
-git pull
+# Peoduction Tessitura Scripts
+#cd /home/pmoody/repositories/tnhs-gocd-scripts
+#p=$(pwd)
+#echo "${magenta}$p"
+#remote=$(git remote -v)
+#echo "${orange}$remote ${reset}"
+#git status
+#git pull
 
 cd /home/pmoody/repositories/Playbooks
 p=$(pwd)
@@ -67,13 +70,14 @@ echo "${orange}$remote ${reset}"
 git status
 git pull 
 
-cd /home/pmoody/repositories/hostingserviceapi
-p=$(pwd)
-echo "${magenta}$p"
-remote=$(git remote -v)
-echo "${orange}$remote ${reset}"
-git status
-git pull
+# This is the self service APU
+#cd /home/pmoody/repositories/hostingserviceapi
+#p=$(pwd)
+#echo "${magenta}$p"
+#remote=$(git remote -v)
+#echo "${orange}$remote ${reset}"
+#git status
+#git pull
 
 cd /home/pmoody/repositories/Python
 p=$(pwd)
@@ -83,7 +87,15 @@ echo "${orange}$remote ${reset}"
 git status
 git pull
 
-cd /home/pmoody/repositories/powershell_private
+cd /home/pmoody/repositories/Terraform
+p=$(pwd)
+echo "${magenta}$p"
+remote=$(git remote -v)
+echo "${orange}$remote ${reset}"
+git status
+git pull
+
+cd /home/pmoody/repositories/BashScripts
 
 
 bash /home/pmoody/repositories/BashScripts/PressAnyKey.sh

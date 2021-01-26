00fdcb2d061c1b2d98f398efd819524593c02feb
Paul Moody <paul.moody@dialogtech.com>
2021-01-26 16:02:03 -0600

    I added the eval and ssh-add lines

--- sshkey.sh
+++ sshkey.sh
@@ -2,3 +2,7 @@
 read -p 'Please enter your password: ' pw
 read -p 'Please enter the file name you want: ' KeyFile
 ssh-keygen -t rsa -b 4096 -C "test@test.com" -P $pw -f "/home/pmoody/.ssh/$KeyFile" -q
+eval "$(ssh-agent -s)"
+ssh-add -K ~/.ssh/$KeyFile
+
+# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent

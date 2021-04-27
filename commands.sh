bf4070f3d5a74f2b42f4479efc8adeecc00d22e2
pmoodyDT <paul.moody@dialogtech.com>
2021-04-27 09:36:16 -0500

    useful commands

---
+++ commands.sh
@@ -0,0 +1,10 @@
+netstat -s # interface statistics
+netstat -a # display all connections
+netstat -at # TCP & UDP connections only
+netstat -i # network interfaces
+netstat -rn # kernet routing info
+netstat -tnl # listening connections only
+
+dig medium.com
+whois medium.com
+

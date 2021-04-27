880191e5a71a5c5a8914877c143cd160201e814e
pmoodyDT <paul.moody@dialogtech.com>
2021-04-27 16:46:42 -0500

    added tcpdump switches

--- commands.sh
+++ commands.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+###  netstat  ###
 netstat -s # interface statistics
 netstat -a # display all connections
 netstat -at # TCP & UDP connections only
@@ -6,6 +8,16 @@ netstat -i # network interfaces
 netstat -rn # kernet routing info
 netstat -tnl # listening connections only
 
+###  web details  ###
 dig medium.com
 whois medium.com
 
+###  tcpdump  ###
+tcpdump -D #list devices
+tcpdump -c #captures x packets
+tcpdump -n #force output to IP
+tcpdump -s #set the capture size
+tcpdump src port
+tcpdump dst port
+tcpdump any port
+tcpdump -w #output to a file

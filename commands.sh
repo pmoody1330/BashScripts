4fb9fd410b2d5086f6caa9a773365f97f3cc092c
pmoodyDT <paul.moody@dialogtech.com>
2021-04-28 10:41:47 -0500

    add scp examples

--- commands.sh
+++ commands.sh
@@ -21,3 +21,19 @@ tcpdump src port
 tcpdump dst port
 tcpdump any port
 tcpdump -w #output to a file
+
+###  scp ###
+# Copy file from a remote host to local host 
+scp pmoody@from_host:file.txt /local/directory/
+
+# Copy file from local host to a remote host 
+scp file.txt pmoody@to_host:/remote/directory/
+
+# Copy directory from a remote host to local host 
+scp -r pmoody@from_host:/remote/directory/  /local/directory/
+
+# Copy directory from local host to a remote host
+scp -r /local/directory/ pmoody@to_host:/remote/directory/
+
+# Copy file from remote host to remote host
+scp pmoody@from_host:/remote/directory/file.txt pmoody@to_host:/remote/directory/
